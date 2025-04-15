2553c3f0831b03c37391784a61df5b27bd2f158f
Tereza Holm <holm.tereza@gmail.com>
2025-04-15 09:16:32 +0200

    Finished part 1.3

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from "react";
 
+const Display = ({ counter }) => <div>{counter}</div>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
   const [counter, setCounter] = useState(0);
   const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
   const setToZero = () => setCounter(0);
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>Plus</button>
-      <button onClick={setToZero}>Reset</button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="Plus" />
+      <Button onClick={decreaseByOne} text="Minus" />
+      <Button onClick={setToZero} text="Restart" />
     </div>
   );
 };
