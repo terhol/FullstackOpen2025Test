2b8d6fd41cc7b1e63b24c7da632991dabc798d4c
Tereza Holm <holm.tereza@gmail.com>
2025-04-16 11:14:59 +0200

    Finished part 1.4

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,27 +1,40 @@
 import { useState } from "react";
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>Press buttons to start using the app.</div>;
+  }
+  return <div>Button press history: {props.allClicks.join(" ")}</div>;
+};
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
 const App = () => {
-  const [clicks, setClicks] = useState({ left: 0, right: 0 });
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  const handleLeftClicks = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 });
+  const handleLeftClicks = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
 
-  const handleRightClicks = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 });
+  const handleRightClicks = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      {clicks.left}
-      <button
-        onClick={() => {
-          handleLeftClicks();
-        }}
-      >
-        left
-      </button>
-      <br />
-      {clicks.right}
-      <button onClick={handleRightClicks}>right</button>
+      <Button onClick={handleLeftClicks} text={"Left"} />
+      <Button onClick={handleRightClicks} text={"Right"} />
+      <History allClicks={allClicks} />
+      <p>Total: {total}</p>
     </div>
   );
 };
