c649d561036e7dcad1a23875e988429c5da4b09f
Tereza Holm <holm.tereza@gmail.com>
2025-04-27 16:24:35 +0200

    Finished part 2.1

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -1,41 +1,16 @@
-import { useState } from "react";
+import Note from "./components/Note";
 
-const History = (props) => {
-  if (props.allClicks.length === 0) {
-    return <div>Press buttons to start using the app.</div>;
-  }
-  return <div>Button press history: {props.allClicks.join(" ")}</div>;
-};
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
-
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0);
-
-  const handleLeftClicks = () => {
-    setAll(allClicks.concat("L"));
-    const updatedLeft = left + 1;
-    setLeft(updatedLeft);
-    setTotal(updatedLeft + right);
-  };
-
-  const handleRightClicks = () => {
-    setAll(allClicks.concat("R"));
-    const updatedRight = right + 1;
-    setRight(updatedRight);
-    setTotal(left + updatedRight);
-  };
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Button onClick={handleLeftClicks} text={"Left"} />
-      <Button onClick={handleRightClicks} text={"Right"} />
-      <History allClicks={allClicks} />
-      <p>Total: {total}</p>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
   );
 };
+
 export default App;
